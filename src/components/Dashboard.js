f62b96844aaa1f4be105e5b19754ced1f587f050
ahza64 <justinmichaelpettit@gmail.com>
2022-05-30 10:17:32 -0700

    robots built with constructor. buttons change location but working on re-render bug.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,77 @@
+import { useState, useEffect } from 'react'
+
 function Dashboard() {
-  const MaintanenceBay = () => {
+  const [robots, setRobots] = useState([{ name: "default" }])
+
+  const Robot = function(idx, name, location) {
+    this.id = idx
+    this.name = name
+    this.location = location
+  }
+
+  useEffect(() => {
+    const numRobots = 10
+    const type = "Bender"
+    const robotsArr = []
+    for (var i = 0; i < numRobots; i++) {
+      robotsArr.push(new Robot(i, `${type} unit ${i + 1}`, "MaintanceBay"))
+      // console.log("robot", robotsArr);
+    }
+    setRobots(robotsArr)
+  }, [])
+
+  function moveRobot(robot, newLocation, robots) {
+    // console.log("moveRobot", robots[robot.id].location, newLocation);
+
+    robots[robot.id].location = newLocation
+    // console.log("newLocation", robots);
+    setRobots(robots)
+  }
+
+  const RobotCard = (props) => {
+
     return (
       <div>
-        MaintanceBay robot list
+        {props.robot.name}
+        <button onClick={() => props.moveRobot(props.robot, "Dept1", props.robots)}>Dept 1</button>
+        <button>Dept 2</button>
+        <button>Maint</button>
       </div>
     )
   }
 
-  const DepartmentOne = () => {
+  const MaintanenceBay = (props) => {
+    // function moveToOne(robot) {
+    //
+    //   console.log("move robot", robot);
+    // }
+
+    return (
+      <div>
+        MaintanceBay robot list
+        {props.robots.map((robot, idx) => {
+          if (robot.location === "MaintanceBay") {
+            return <RobotCard key={robot.name} robot={robot} moveRobot={props.moveRobot} robots={props.robots}/>
+          }
+        })}
+      </div>
+    )
+  }
+
+  const DepartmentOne = (props) => {
+    // function moveToOne(robot) {
+    //
+    //   console.log("move robot", robot);
+    // }
+
     return (
       <div>
         Department one robot list
+        {props.robots.map((robot, idx) => {
+          if (robot.location === "Dept1") {
+            return <RobotCard key={robot.name} robot={robot} />
+          }
+        })}
       </div>
     )
   }
@@ -26,9 +87,9 @@ function Dashboard() {
   return (
     <div>
       robot lists
-      <MaintanenceBay />
-      <DepartmentOne />
-      <DepartmentTwo />
+      <MaintanenceBay robots={robots} moveRobot={moveRobot} />
+      <DepartmentOne robots={robots} />
+      <DepartmentTwo robots={robots} />
     </div>
   )
 }
